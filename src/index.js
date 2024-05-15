718a648592e57785595f7bd058dd11a278870cbe
Bill C. <106033471+bi1cor0@users.noreply.github.com>
2024-05-15 11:55:10 -0700

    Added toolbar and color via Javascript code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 
 var mainEl = document.getElementsByTagName(`main`);
 console.log(mainEl[0]);
-let backColor = 
+
 mainEl[0].style.backgroundColor = 'var(--main-bg)';
 mainEl[0].innerHTML = `<h1>DOM Manipulation</h>`;
 mainEl[0].classList.add(`flex-ctr`);
+
+var topMenuEl = document.getElementById(`top-menu`)
+console.log(topMenuEl);
+
+topMenuEl.style.height = `100%`
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
+topMenuEl.classList.add(`flex-around`)
