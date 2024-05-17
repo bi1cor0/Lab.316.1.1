9e469892d6f89d247f621c7bd55c08d8e7e64f8b
Bill C. <106033471+bi1cor0@users.noreply.github.com>
2024-05-17 14:41:04 -0700

    Revisiting lab to continue on it for the weighted lab.

--- src/index.js
+++ src/index.js
@@ -17,14 +17,41 @@ topMenuEl.classList.add(`flex-around`)
 //Part 3
 // Menu data structure
 var menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
-  ];
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
 menuLinks.forEach((Link) => {
     let newLink = document.createElement(`a`)
     newLink.setAttribute(`href`, Link.href)
     newLink.textContent = Link.text
     topMenuEl.appendChild(newLink)
 })
+
+//Part 4
+//Set up sub toolbar menu for a dropdown menu
+
+var subMenuEl = document.getElementById(`sub-menu`);
+
+subMenuEl.style.height = `100%`;
+subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)'
+subMenuEl.style.position = 'absolute'
+subMenuEl.style.top = '0'
+subMenuEl.classList.add(`flex-around`);
+
+//Part 5
+//adding interactivity
+var topMenuLinks = document.body.getElementsByTagName(`a`)
+console.log(topMenuLinks)
